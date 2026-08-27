2bc3dfaf26975514ba583a76200f32598acab65e
Txiaoshen <txiaoshen666@gmail.com>
2026-08-27 16:14:43 +0800

    fix(home): import ImeAction for keyboardOptions

--- app/src/main/java/com/example/githubappstore/ui/home/HomeRoute.kt
+++ app/src/main/java/com/example/githubappstore/ui/home/HomeRoute.kt
@@ -30,6 +30,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.githubappstore.GitHubAppStoreApp
